5e555a68178ad0340deae063334858beebd0d00f
CloudsCloudsClouds <56483061+CloudsCloudsClouds@users.noreply.github.com>
2025-11-04 19:08:29 -0400

    Better yet singleton

--- autoloads/player_manager.gd
+++ autoloads/player_manager.gd
@@ -1,29 +1,87 @@
 extends Node
 
 signal player_joined(player_slot, device_id)
-
 signal player_left(player_slot)
 
+# Maps: Player Slot -> Data Dictionary
+# { 0: {"device": 5, "score": 10}, 1: {"device": -1, "score": 0} }
 var player_data: Dictionary = {}
+
+# CRITICAL FIX 1: Add a reverse map.
+# Maps: Device ID -> Player Slot
+# { 5: 0, -1: 1 }
+# This makes lookups *much* faster.
+var device_to_slot_map: Dictionary = {}
 
 const MAX_PLAYER = 8
 
+func _ready():
+	# CRITICAL FIX 2: Handle disconnects.
+	Input.joy_connection_changed.connect(_on_joy_connection_changed)
+
+# --- Public API ---
+
 func join(device: int) -> void:
-	var player = next_viable_player()
+	# 1. Check if device is already joined (now fast)
+	if device_to_slot_map.has(device):
+		return
+
+	# 2. Find a slot
+	var player_slot = next_viable_player()
+	if player_slot < 0:
+		return # Game is full
+
+	# 3. Update *both* maps
+	player_data[player_slot] = {
+		"device": device
+		# other data...
+	}
+	device_to_slot_map[device] = player_slot
 	
-	if player >= 0:
-		player_data[player] = {
-			"device": device,
-			# other to come
-		}
-		player_joined.emit(player, device)
+	# 4. Tell the plugin to map this.
+	# THIS IS THE MISSING LINK TO YOUR PLUGIN.
+	# The name will be different based on your plugin.
+	MultiplayerInput.assign_device_to_slot(device, player_slot)
+	
+	# 5. Emit signal
+	player_joined.emit(player_slot, device)
 
+func leave(player_slot: int) -> void:
+	if !player_data.has(player_slot):
+		return # This slot is already empty
 
-func leave(player: int) -> void:
-	if player_data.has(player):
-		player_data.erase(player)
-		player_left.emit(player)
+	# 1. Get device ID *before* erasing
+	var device = get_player_device(player_slot)
 
+	# 2. Erase from both maps
+	player_data.erase(player_slot)
+	if device_to_slot_map.has(device): # Safety check
+		device_to_slot_map.erase(device)
+	
+	# 3. Tell the plugin to unmap this.
+	MultiplayerInput.unassign_slot(player_slot)
+
+	# 4. Emit signal
+	player_left.emit(player_slot)
+
+# --- Input Handlers ---
+
+func handle_join_input():
+	for device in get_unjoined_devices():
+		# This assumes your plugin is set up to map a "join" action
+		# to all unassigned devices.
+		if MultiplayerInput.is_action_just_pressed(device, "join"):
+			join(device)
+
+# You may also want a way for players to *choose* to leave
+func handle_leave_input():
+	for player_slot in get_player_indexes():
+		var device = get_player_device(player_slot)
+		# Assumes "ui_cancel" is mapped to the player's slot (p1_ui_cancel, etc.)
+		if MultiplayerInput.is_action_just_pressed(device, "ui_cancel"):
+			leave(player_slot)
+
+# --- Getters / Setters ---
 
 func get_player_count():
 	return player_data.size()
@@ -31,57 +89,47 @@ func get_player_count():
 func get_player_indexes():
 	return player_data.keys()
 
-func get_player_device(player: int) -> int:
-	return get_player_data(player, "device")
-
-# get player data.
-# null means it doesn't exist.
-func get_player_data(player: int, key: StringName):
-	if player_data.has(player) and player_data[player].has(key):
-		return player_data[player][key]
-	return null
-
-# set player data to get later
-func set_player_data(player: int, key: StringName, value: Variant):
-	# if this player is not joined, don't do anything:
-	if !player_data.has(player):
-		return
-	
-	player_data[player][key] = value
-
-# call this from a loop in the main menu or anywhere they can join
-# this is an example of how to look for an action on all devices
-func handle_join_input():
-	for device in get_unjoined_devices():
-		if MultiplayerInput.is_action_just_pressed(device, "join"):
-			join(device)
-
-# to see if anybody is pressing the "start" action
-# this is an example of how to look for an action on all players
-# note the difference between this and handle_join_input(). players vs devices.
-func someone_wants_to_start() -> bool:
-	for player in player_data:
-		var device = get_player_device(player)
-		if MultiplayerInput.is_action_just_pressed(device, "start"):
-			return true
-	return false
-
+# This is now fast and simple
 func is_device_joined(device: int) -> bool:
-	for player_id in player_data:
-		var d = get_player_device(player_id)
-		if device == d: return true
-	return false
+	return device_to_slot_map.has(device)
+
+# This is fast and simple
+func get_slot_from_device(device: int) -> int:
+	return device_to_slot_map.get(device, -1) # Returns -1 if not found
+
+func get_unjoined_devices():
+	var devices = Input.get_connected_joypads()
+	devices.append(-1) # Add keyboard
+	
+	# Now *much* faster.
+	return devices.filter(func(device): return !is_device_joined(device))
 
 func next_viable_player() -> int:
 	for i in MAX_PLAYER:
 		if !player_data.has(i): return i
 	return -1
 
-# returns an array of all valid devices that are *not* associated with a joined player
-func get_unjoined_devices():
-	var devices = Input.get_connected_joypads()
-	# also consider keyboard player
-	devices.append(-1)
-	
-	# filter out devices that are joined:
-	return devices.filter(func(device): return !is_device_joined(device))
+# --- Signal Callback ---
+
+func _on_joy_connection_changed(device: int, connected: bool):
+	if !connected:
+		# Device disconnected. Check if it was a player.
+		var player_slot = get_slot_from_device(device)
+		if player_slot != -1:
+			# It was a player. Make them leave.
+			leave(player_slot)
+
+# --- (Your other helper functions like get/set_player_data are fine) ---
+
+func get_player_device(player: int) -> int:
+	return get_player_data(player, "device")
+
+func get_player_data(player: int, key: StringName):
+	if player_data.has(player) and player_data[player].has(key):
+		return player_data[player][key]
+	return null
+
+func set_player_data(player: int, key: StringName, value: Variant):
+	if !player_data.has(player):
+		return
+	player_data[player][key] = value
