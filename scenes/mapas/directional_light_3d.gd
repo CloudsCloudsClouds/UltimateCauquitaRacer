027fbf2228b413a404f64784f8e8ae5265f33cdb
Fabricio44S <fabriciosebastian2000@gmail.com>
2025-12-09 17:50:37 -0400

    agregando podio

--- scenes/mapas/directional_light_3d.gd
+++ scenes/mapas/directional_light_3d.gd
@@ -5,4 +5,4 @@ extends DirectionalLight3D
 func _process(delta: float) -> void:
 	# Gira la luz alrededor del eje X (amanecer/atardecer)
 	var angle := deg_to_rad(rot_speed_deg) * delta
-	rotate_y(angle)
+	rotate_z(angle)
