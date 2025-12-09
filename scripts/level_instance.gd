6a0725be3da74fff1ca34e95b58d362994182ffa
CloudsCloudsClouds <56483061+CloudsCloudsClouds@users.noreply.github.com>
2025-12-09 16:56:57 -0400

    ultimate round thing

--- scripts/level_instance.gd
+++ scripts/level_instance.gd
@@ -3,4 +3,4 @@ extends Node3D
 
 @export var car_spawn_area: CollisionShape3D
 @export var power_spawn_area: CollisionShape3D
-@export var view_camera: CollisionShape3D
+@export var view_camera: CameraFollower
